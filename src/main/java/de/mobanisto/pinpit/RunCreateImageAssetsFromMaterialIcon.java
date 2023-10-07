4420c4297546a442dad592e22bb3fffbfddeaeb9
Sebastian Kürten <sebastian@topobyte.de>
2023-10-07 16:01:23 +0200

    Allow web colors as arguments for colors on CLI

--- src/main/java/de/mobanisto/pinpit/RunCreateImageAssetsFromMaterialIcon.java
+++ src/main/java/de/mobanisto/pinpit/RunCreateImageAssetsFromMaterialIcon.java
@@ -2,12 +2,19 @@ package de.mobanisto.pinpit;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 
+import com.google.common.base.Joiner;
+
 import de.mobanisto.pinpit.tasks.CreateImageAssetsFromMaterialIcon;
 import de.topobyte.chromaticity.ColorCode;
+import de.topobyte.chromaticity.WebColors;
 import de.topobyte.utilities.apache.commons.cli.OptionHelper;
 import de.topobyte.utilities.apache.commons.cli.commands.args.CommonsCliArguments;
 import de.topobyte.utilities.apache.commons.cli.commands.options.CommonsCliExeOptions;
@@ -23,6 +30,27 @@ public class RunCreateImageAssetsFromMaterialIcon
 	private static final String OPTION_COLOR_FOREGROUND = "color-foreground";
 	private static final String OPTION_COLOR_DIALOG = "color-dialog";
 
+	private static Map<String, ColorCode> webColors = new HashMap<>();
+	static {
+		for (WebColors color : WebColors.values()) {
+			webColors.put(color.name().toLowerCase(), color.color());
+		}
+	}
+
+	private static String colorHelpMessage;
+	static {
+		StringBuilder strb = new StringBuilder();
+		strb.append(
+				"Colors can be specified using hex notation such as 0xaaff22");
+		strb.append(" or by name as one of the web colors: ");
+
+		Stream<String> colorNames = Arrays.asList(WebColors.values()).stream()
+				.map(e -> e.name().toLowerCase());
+		Joiner.on(", ").appendTo(strb, colorNames.iterator());
+
+		colorHelpMessage = strb.toString();
+	}
+
 	public static ExeOptionsFactory OPTIONS_FACTORY = new ExeOptionsFactory() {
 
 		@Override
@@ -36,7 +64,16 @@ public class RunCreateImageAssetsFromMaterialIcon
 			OptionHelper.addL(options, OPTION_COLOR_FOREGROUND, true, false, "color", "color for tinting the Material icon");
 			OptionHelper.addL(options, OPTION_COLOR_DIALOG, true, false, "color", "background used in the Windows installer dialog");
 			// @formatter:on
-			return new CommonsCliExeOptions(options, "[options]");
+
+			String nl = System.getProperty("line.separator");
+
+			StringBuilder strb = new StringBuilder();
+			strb.append("[options]");
+			strb.append(nl + nl);
+			strb.append(colorHelpMessage);
+			strb.append(nl + nl);
+
+			return new CommonsCliExeOptions(options, strb.toString());
 		}
 
 	};
@@ -48,11 +85,21 @@ public class RunCreateImageAssetsFromMaterialIcon
 
 		Path input = Paths.get(line.getOptionValue(OPTION_INPUT));
 		Path output = Paths.get(line.getOptionValue(OPTION_OUTPUT));
-		ColorCode colorBackground = color(
-				line.getOptionValue(OPTION_COLOR_BACKGROUND));
-		ColorCode colorForeground = color(
-				line.getOptionValue(OPTION_COLOR_FOREGROUND));
-		ColorCode colorDialog = color(line.getOptionValue(OPTION_COLOR_DIALOG));
+		ColorCode colorBackground = null;
+		ColorCode colorForeground = null;
+		ColorCode colorDialog = null;
+		try {
+			colorBackground = color(
+					line.getOptionValue(OPTION_COLOR_BACKGROUND));
+			colorForeground = color(
+					line.getOptionValue(OPTION_COLOR_FOREGROUND));
+			colorDialog = color(line.getOptionValue(OPTION_COLOR_DIALOG));
+		} catch (NumberFormatException e) {
+			System.out.println("Error: Invalid color specified.");
+			System.out.println();
+			System.out.println(colorHelpMessage);
+			System.exit(0);
+		}
 
 		CreateImageAssetsFromMaterialIcon task = new CreateImageAssetsFromMaterialIcon(
 				input, output, colorBackground, colorForeground, colorDialog);
@@ -63,6 +110,9 @@ public class RunCreateImageAssetsFromMaterialIcon
 	{
 		if (value == null) {
 			return null;
+		}
+		if (webColors.containsKey(value.toLowerCase())) {
+			return webColors.get(value.toLowerCase());
 		}
 		int hex = parseHex(value);
 		return new ColorCode(hex);
